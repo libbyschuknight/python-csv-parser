0dc73eff7315e11b3aa21aa63741bd5e1cdf8d35
libbyschuknight <libbysk@gmail.com>
2017-08-06 12:18:40 +1200

    Get column data, strip whitespace

--- user_upload.py
+++ user_upload.py
@@ -4,6 +4,9 @@
 import csv
 
 with open('users.csv', 'rb') as csvfile:
-    users_reader = csv.reader(csvfile, delimiter=',')
+    users_reader = csv.DictReader(csvfile)
     for row in users_reader:
-        print row
+        name = row['name'].strip()
+        surname = row['surname'].strip()
+        email_address = row['email\t'].strip()
+        print (name, surname, email_address)
