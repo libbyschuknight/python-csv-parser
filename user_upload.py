89920bf098140511c40fc167109d47dc5903be50
libbyschuknight <libbysk@gmail.com>
2017-08-06 12:26:29 +1200

    Make correct case, remove '!'

--- user_upload.py
+++ user_upload.py
@@ -6,7 +6,7 @@ import csv
 with open('users.csv', 'rb') as csvfile:
     users_reader = csv.DictReader(csvfile)
     for row in users_reader:
-        name = row['name'].strip()
-        surname = row['surname'].strip()
-        email_address = row['email\t'].strip()
+        name = row['name'].strip().title().replace('!', '')
+        surname = row['surname'].strip().title().replace('!', '')
+        email_address = row['email\t'].strip().lower()
         print (name, surname, email_address)
